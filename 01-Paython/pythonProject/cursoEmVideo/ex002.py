c71bf63f9c49b6182097f597e76e574489bf7de1
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-09 17:01:33 -0300

    tipos em py

---
+++ 01-Paython/pythonProject/cursoEmVideo/ex002.py
@@ -0,0 +1,22 @@
+# Tipos e entrada de dados
+
+# Strings
+bv = 'Seja bem vindo!'
+nome = input('Digite seu nome: ')
+
+# Interpolação de Strings
+print(f'{nome}, {bv}')
+# ou
+print('{}, {}'.format(nome, bv))
+
+# Numbers
+nInt = int(input('Tipo Int: '))
+print(nInt)
+nFloat = float(input('Tipo Float: '))
+print(nFloat)
+
+# Boolean
+vBool = bool(input('Valor Boolean: '))
+print(vBool)
+
+
---
+++ 01-Paython/pythonProject/cursoEmVideo/ex003.py
@@ -0,0 +1,1 @@
+# https://www.youtube.com/watch?v=PB254Cfjlyk&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=10
