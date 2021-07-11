28e13326e8f45e5e892649c796f470a4aa660974
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-11 14:28:16 -0300

    POO aula 4

--- 01-Paython/pythonProject/CoreySchafer-POO/V4.py
+++ 01-Paython/pythonProject/CoreySchafer-POO/V4.py
@@ -1,8 +1,6 @@
 # TODO Aula 04: https://www.youtube.com/watch?v=RSl87lqOXDE&list=PL-osiE80TeTsqhIuOqKhwlXsIBIdSeYtc&index=4
 
 class Employee:
-
-    num_instances = 0
     percent = 1.04
 
     def __init__(self, name, lastname, sal):
@@ -11,35 +9,65 @@ class Employee:
         self.sal = sal
         self.email = '{}.{}@prop.com'.format(name, lastname)
 
-        # count
-        Employee.num_instances += 1
-
-    def fullUser(self):
+    def fullname(self):
         return '{} {}'.format(self.name, self.lastname)
 
-    def increase(self):
+    def apply_raise(self):
         self.sal = int(self.sal * self.percent)
 
-    def increase_manager(self):
-        Employee.percent = 1.10
-        self.sal = int(self.sal * self.percent)
 
-    @classmethod
-    def set_raise_amount(cls, amount):# cls -> abreviação de class
-        cls.percent = amount
+class Devs(Employee):
+    percent = 1.10
 
-    @classmethod
-    def split_string(cls, emp_str):# trabalhando com Split()
-        name, lastname, sal = emp_str.split('-')
-        return cls(name, lastname, sal)
-
-    @staticmethod
-    def workday(day):# trabalhando com datas
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+    def __init__(self, name, lastname, sal, program_lang):
+        super().__init__(name, lastname, sal)
+        self.program_lang = program_lang
 
 
-emp_1 = Employee('Jef', 'Melo', 70000)
-emp_2 = Employee('Jon', 'Car', 50000)
+class Manager(Employee):
 
+    def __init__(self, name, lastname, sal, employees=None):
+        super().__init__(name, lastname, sal)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_employ(self, employ):
+        if employ not in self.employees:
+            self.employees.append(employ)
+
+    def remove_employ(self, employ):
+        if employ in self.employees:
+            self.employees.remove(employ)
+
+    def print_employees(self):
+        for employ in self.employees:
+            print('-->', employ.fullname())
+
+
+# print(help(Devs))  # Retorna os Atributos da Classe
+
+dev_1 = Devs('Jef', 'Melo', 70000, 'Kotlin')
+dev_2 = Devs('Jon', 'Car', 50000, 'Python')
+
+m_1 = Manager('Gerente', 'Gen', 90000, [dev_1])
+
+print(m_1.name)
+# m_1.print_employees()
+
+m_1.add_employ(dev_2)
+# m_1.print_employees()
+
+m_1.remove_employ(dev_1)
+m_1.print_employees()
+
+print(isinstance(m_1, Employee))# Retorna um bool se extend ou não de x class
+print(issubclass(m_1, Employee))# Retorna um bool se é ou não uma sub class de
+
+# print(dev_1.sal)
+# dev_1.apply_raise()
+# print(dev_1.sal)
+# print(dev_1.email)
+# print(dev_1.program_lang)
+# print(dev_2.program_lang)
