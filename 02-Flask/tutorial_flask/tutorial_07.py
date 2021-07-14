2618668d3d7be898d1f9afe4b2949e7a3963ea47
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 18:10:59 -0300

    commit inicial tutorial 07

---
+++ 02-Flask/tutorial_flask/tutorial_07.py
@@ -0,0 +1,47 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from datetime import timedelta
+
+app = Flask(__name__)
+app.secret_key = "jefmelo"
+app.permanent_session_lifetime = timedelta(minutes=2)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["name"]
+        session["user"] = user
+        flash("Login Succesful")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in")
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return render_template("user.html", user=user)
+    else:
+        flash("You are not logged in")
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    flash(f"You have been logged out", "info")
+    session.pop("user", None)
+    
+    return redirect(url_for("login"))
+
+if __name__ == "__main__":
+    app.run(debug=True) 
