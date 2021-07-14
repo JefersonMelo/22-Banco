5283b786252d67b8f00248779f72780d5ba8c08f
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 14:26:49 -0300

    tutorial 03

--- 02-Flask/tutorial_flask/tutorial_03.py
+++ 02-Flask/tutorial_flask/tutorial_03.py
@@ -3,9 +3,13 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", context=["Jef","Jon","Car"])
+@app.route("/")
+def home():
+    return render_template("index_tut_03.html")
+
+@app.route("/02")
+def tut_02():
+    return render_template("index_tut_02.html")
 
 if __name__ == "__main__":
-    app.run() 
+    app.run(debug=True) 
