7e58812231a5cb65b47d760a39d22a92f3e8fb69
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 16:32:01 -0300

    tutorial 04

--- 02-Flask/tutorial_flask/tutorial_04.py
+++ 02-Flask/tutorial_flask/tutorial_04.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("tutorial_04.html")
+    return render_template("index.html")
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
