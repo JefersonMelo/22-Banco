9f2bec20db83beb4806f9b84cd6402c513e2f098
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 15:02:22 -0300

    commit inicial tutorial 05

---
+++ 02-Flask/tutorial_flask/tutorial_05.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("tutorial_05.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["name"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True) 
