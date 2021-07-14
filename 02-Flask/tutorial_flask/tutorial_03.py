8b1edca83c8f4b7961d67ed58997019da6261d7b
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 14:32:17 -0300

    tutorial_03

--- 02-Flask/tutorial_flask/tutorial_03.py
+++ 02-Flask/tutorial_flask/tutorial_03.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index_tut_03.html")
+    return render_template("tutorial_03.html")
 
 @app.route("/02")
 def tut_02():
-    return render_template("index_tut_02.html")
+    return render_template("tutorial_02.html")
 
 if __name__ == "__main__":
     app.run(debug=True) 
