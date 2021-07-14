f5025ed580871880821ba0bd584d241da48afd5f
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 13:22:39 -0300

    commit incial tutorial 03

---
+++ 02-Flask/tutorial_flask/tutorial_03.py
@@ -0,0 +1,11 @@
+from os import name
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", context=["Jef","Jon","Car"])
+
+if __name__ == "__main__":
+    app.run() 
