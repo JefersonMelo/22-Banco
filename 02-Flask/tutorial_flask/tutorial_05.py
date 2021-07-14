f6dfd0440862ba954620123c3c746247a31167cb
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-14 16:31:35 -0300

    tutorial 05

--- 02-Flask/tutorial_flask/tutorial_05.py
+++ 02-Flask/tutorial_flask/tutorial_05.py
@@ -1,22 +1,42 @@
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
+app.secret_key = ":)"
+app.permanent_session_lifetime = timedelta(minutes=2)
 
 @app.route("/")
 def home():
-    return render_template("tutorial_05.html")
+    return render_template("index.html")
+
 
 @app.route("/login", methods=["POST", "GET"])
 def login():
     if request.method == "POST":
+        session.permanent = True
         user = request.form["name"]
-        return redirect(url_for("user", usr=user))
+        session["user"] = user
+        return redirect(url_for("user"))
     else:
+        if "user" in session:
+            return redirect(url_for("user"))
+
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True) 
