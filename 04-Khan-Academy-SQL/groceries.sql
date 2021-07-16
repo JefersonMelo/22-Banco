0d726a7f2470f5b4f3e18f43514776af16269f36
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-16 17:18:55 -0300

    aula 01

---
+++ 04-Khan-Academy-SQL/groceries.sql
@@ -0,0 +1,12 @@
+CREATE TABLE groceries(
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    qtd INTEGER
+);
+
+INSERT INTO groceries VALUES (1, "Bananas", 4);
+INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
+INSERT INTO groceries VALUES (3, "Dark chocolate bars", 2);
+
+SELECT * FROM groceries;
+
