366b9e7a1c9d2d1c0b4ed626bc4bd28c734eacc6
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-20 10:27:19 -0300

    rename for organization

--- 04-Khan-Academy-SQL/sql/02-Consultas-SQL-mais-avançadas/01-Explanation/01-exercise_logs.sql
+++ 04-Khan-Academy-SQL/sql/02-Consultas-SQL-mais-avançadas/01-Explanation/01-exercise_logs.sql
@@ -1,80 +1,27 @@
-CREATE TABLE exercise_logs(id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE exercise_logs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     type TEXT,
     minutes INTEGER, 
     calories INTEGER,
     heart_rate INTEGER
 );
 
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("biking", 30, 100, 110);
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("biking", 10, 30, 105);
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("dancing", 15, 200, 120);
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("tree climbing", 30, 70, 90);
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("tree climbing", 25, 72, 80);
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("rowing", 30, 70, 90);
-INSERT INTO exercise_logs(type, minutes, calories, heart_rate) 
-    VALUES ("hiking", 60, 80, 85);
 
-SELECT * 
-FROM exercise_logs 
-WHERE type = "biking";
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("biking", 30, 100, 110);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("biking", 10, 30, 105);
+INSERT INTO exercise_logs(type, minutes, calories, heart_rate) VALUES ("dancing", 15, 200, 120);
 
-SELECT * 
-FROM exercise_logs 
-WHERE type = "biking" 
-OR type = "hiking" 
-OR type = "tree climbing" 
-OR type = "rowing";
+SELECT * FROM exercise_logs;
 
-/* IN */
-SELECT * 
-FROM exercise_logs 
-WHERE type IN ("biking", "hiking", "tree climbing", "rowing");
+SELECT * FROM exercise_logs 
+WHERE calories > 50 
+ORDER BY calories;
 
-/* NOT IN */
-SELECT * 
-FROM exercise_logs 
-WHERE type NOT IN ("biking", "hiking", "tree climbing", "rowing");
+/* AND */
+SELECT * FROM exercise_logs 
+WHERE calories > 50 
+ORDER BY calories;
 
-CREATE TABLE drs_favorites(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    type TEXT,
-    reason TEXT
-);
-
-INSERT INTO drs_favorites(type, reason) 
-    VALUES ("biking", "Improves endurance and flexibility.");
-INSERT INTO drs_favorites(type, reason) 
-    VALUES ("hiking", "Increases cardiovascular health.");
-
-
-SELECT type 
-FROM drs_favorites;
-
-SELECT * 
-FROM exercise_logs 
-WHERE type IN (
-    SELECT type FROM drs_favorites
-);
-
-SELECT * 
-FROM exercise_logs 
-WHERE type IN (
-    SELECT type FROM drs_favorites
-    WHERE reason = "Increases cardiovascular health."
-);
-
-/* LIKE */
-SELECT * 
-FROM exercise_logs 
-WHERE type IN (
-    SELECT type FROM drs_favorites
-    WHERE reason LIKE "%cardiovascular%"
-);
-
-
+/* OR */
+SELECT * FROM exercise_logs 
+WHERE calories > 50 OR heart_rate > 100;
