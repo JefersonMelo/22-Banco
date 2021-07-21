4158579498a7d7b700edf8c2a15eb678e6e59789
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-21 18:41:57 -0300

    correção link

---
+++ 04-Khan-Academy-SQL/sql/04-Modificando-bancos-de-dados-com-SQL/04-app.sql
@@ -0,0 +1,15 @@
+/*Projeto: App personificador*/
+
+/*
+Pense em seus aplicativos preferidos e escolha um que armazena seus dados, 
+por exemplo um jogo que armazena pontuação, um aplicativo que permite que 
+você publique atualizações, etc. Neste projeto você vai imaginar que o aplicativo 
+armazena seus dados em um banco de dados SQL (o que é muito provável!) e 
+escrever instruções SQL que poderiam ser as usadas pelo aplicativo.
+
+    CREATE - crie uma tabela para armazenar os dados.
+    INSERT - insira algumas linhas de exemplo na tabela.
+    Use an UPDATE - atualize para simular o que acontece quando você edita os dados no aplicativo.
+    Use a DELETE - apague para simular o que acontece quando você exclui os dados no aplicativo.
+*/
+
