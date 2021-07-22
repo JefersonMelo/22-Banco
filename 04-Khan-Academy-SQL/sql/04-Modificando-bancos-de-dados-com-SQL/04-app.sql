917e872fc57e05f4de7b3a4525bc97012b364140
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-22 10:28:26 -0300

    Projeto: App personificador

--- 04-Khan-Academy-SQL/sql/04-Modificando-bancos-de-dados-com-SQL/04-app.sql
+++ 04-Khan-Academy-SQL/sql/04-Modificando-bancos-de-dados-com-SQL/04-app.sql
@@ -13,3 +13,33 @@ escrever instruções SQL que poderiam ser as usadas pelo aplicativo.
     Use a DELETE - apague para simular o que acontece quando você exclui os dados no aplicativo.
 */
 
+CREATE TABLE celebrity(
+    id INTEGER PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    famous_profession TEXT NOT NULL,
+    age INTEGER,
+    nationality TEXT,
+    charact TEXT
+);
+
+INSERT INTO celebrity (id, first_name, last_name, famous_profession, age, nationality, charact)
+    VALUES (1, "Emma", "Watson", "actress", 31, "Paris-France", "Hermione Granger");
+
+INSERT INTO celebrity (id, first_name, last_name, famous_profession, age, nationality, charact)
+    VALUES (2, "Maisie", "Williams", "actress", 24, "Bristol-UK", "Arya Stark");
+
+INSERT INTO celebrity (id, first_name, last_name, famous_profession, age, nationality, charact)
+    VALUES (3, "Emilia", "Clarke", "actress", 34, "Berkshire-UK", "Daenerys Targaryen");
+
+ALTER TABLE celebrity
+ADD salary REAL
+DEFAULT 0.00;
+
+UPDATE celebrity
+SET salary = 100000.0
+WHERE id = 1;
+
+DELETE
+FROM celebrity
+WHERE id = 2;
