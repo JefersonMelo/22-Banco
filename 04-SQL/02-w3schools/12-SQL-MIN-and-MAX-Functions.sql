95f90898390d1165ce0a2174f4952be4ea9363d5
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-23 18:16:07 -0300

    SQL MIN() and MAX() Functions

---
+++ 04-SQL/02-w3schools/12-SQL-MIN-and-MAX-Functions.sql
@@ -0,0 +1,12 @@
+/*SQL MIN() and MAX() Functions*/
+
+/*MIN() Example*/
+SELECT MIN(Price) 
+AS SmallestPrice
+FROM Products;
+
+/*MAX() Example*/
+SELECT MAX(Price) 
+AS LargestPrice
+FROM Products; 
+
