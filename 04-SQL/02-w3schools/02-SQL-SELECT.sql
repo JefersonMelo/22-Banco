679f530421aa233827dd2f5cfcbf48c2b5ba105e
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-23 16:41:09 -0300

    02-SQL-SELECT

---
+++ 04-SQL/02-w3schools/02-SQL-SELECT.sql
@@ -0,0 +1,11 @@
+/*The SQL SELECT Statement*/
+
+SELECT * FROM table_name; 
+
+/*SELECT Column Example*/
+SELECT CustomerName, City 
+FROM Customers;
+
+/*SELECT * Example*/
+SELECT * 
+FROM Customers;
