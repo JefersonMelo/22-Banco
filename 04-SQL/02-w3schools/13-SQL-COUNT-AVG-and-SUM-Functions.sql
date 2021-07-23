e5b5277302f66e2868cee4d1a500d1fc1dcd91b9
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-23 18:18:41 -0300

    SQL COUNT(), AVG() and SUM() Functions

---
+++ 04-SQL/02-w3schools/13-SQL-COUNT-AVG-and-SUM-Functions.sql
@@ -0,0 +1,14 @@
+/*SQL COUNT(), AVG() and SUM() Functions*/
+
+/*COUNT() Example*/
+SELECT COUNT(ProductID)
+FROM Products;
+
+/*AVG() Example*/
+SELECT AVG(Price)
+FROM Products;
+
+/*SUM() Example*/
+SELECT SUM(Quantity)
+FROM OrderDetails; 
+
