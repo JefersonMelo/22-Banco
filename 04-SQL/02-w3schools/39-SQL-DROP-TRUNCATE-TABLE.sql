c557e08b91e4966f3e07c37170778694bce1991d
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-27 09:45:33 -0300

    SQL DROP AND TRUNCATE TABLE

---
+++ 04-SQL/02-w3schools/39-SQL-DROP-TRUNCATE-TABLE.sql
@@ -0,0 +1,9 @@
+/*SQL DROP AND TRUNCATE TABLE*/
+
+/*SQL DROP TABLE Example
+The following SQL statement drops the existing table "Shippers":*/
+DROP TABLE Shippers;
+
+/*SQL TRUNCATE TABLE
+The TRUNCATE TABLE statement is used to delete the data inside a table, but not the table itself.*/
+TRUNCATE TABLE Shippers;
