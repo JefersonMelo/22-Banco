78965da456c671381a69572618e5ad617bde2e3c
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-27 08:54:35 -0300

    SQL Stored Procedures for SQL Server

---
+++ 04-SQL/02-w3schools/34-SQL-Stored-Procedures-for-SQL-Server.sql
@@ -0,0 +1,40 @@
+/*SQL Stored Procedures for SQL Server*/
+
+/*Stored Procedure Example
+The following SQL statement creates a stored procedure named 
+"SelectAllCustomers" that selects all records from the "Customers" table:*/
+CREATE PROCEDURE SelectAllCustomers
+AS
+SELECT * FROM Customers
+GO;
+
+/*Execute the stored procedure above as follows:*/
+EXEC SelectAllCustomers;
+
+/*Stored Procedure With One Parameter
+The following SQL statement creates a stored procedure that selects Customers 
+from a particular City from the "Customers" table:*/
+CREATE PROCEDURE SelectAllCustomers 
+    @City nvarchar(30)
+AS
+SELECT * FROM Customers WHERE City = @City
+GO;
+
+/*Execute the stored procedure above as follows:*/
+EXEC SelectAllCustomers @City = 'London'; 
+
+/*Stored Procedure With Multiple Parameters
+The following SQL statement creates a stored procedure that selects Customers 
+from a particular City with a particular PostalCode from the "Customers" table:*/
+CREATE PROCEDURE SelectAllCustomers 
+    @City nvarchar(30), 
+    @PostalCode nvarchar(10)
+AS
+SELECT * FROM Customers 
+WHERE City = @City 
+AND PostalCode = @PostalCode
+GO;
+
+/*Execute the stored procedure above as follows:*/
+EXEC SelectAllCustomers 
+@City = 'London', @PostalCode = 'WA1 1DP'; 
