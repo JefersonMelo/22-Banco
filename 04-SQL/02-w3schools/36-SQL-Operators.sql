0990888e1a873d8941d05c3e48e7116570f8d04d
Jeferson Melo <jefsdemelo@gmail.com>
2021-07-27 09:29:10 -0300

    SQL Operators

---
+++ 04-SQL/02-w3schools/36-SQL-Operators.sql
@@ -0,0 +1,53 @@
+/*SQL Operators*/
+
+/*SQL Arithmetic Operators
+    Operator 	Description
+    + 	        Add 	
+    - 	        Subtract 	
+    * 	        Multiply 	
+    / 	        Divide 	
+    % 	        Modulo
+*/
+
+/*SQL Bitwise Operators
+    Operator 	Description
+    & 	        Bitwise AND
+    | 	        Bitwise OR
+    ^ 	        Bitwise exclusive OR
+*/
+
+/*SQL Comparison Operators
+    Operator 	Description 
+    = 	        Equal to 	
+    > 	        Greater than 	
+    < 	        Less than 	
+    >= 	        Greater than or equal to 	
+    <= 	        Less than or equal to 	
+    <> 	        Not equal to
+*/
+
+/*SQL Compound Operators
+    Operator 	Description
+    += 	        Add equals
+    -= 	        Subtract equals
+    *= 	        Multiply equals
+    /= 	        Divide equals
+    %= 	        Modulo equals
+    &= 	        Bitwise AND equals
+    ^-= 	    Bitwise exclusive equals
+    |*= 	    Bitwise OR equals
+*/
+
+/*SQL Logical Operators
+    Operator 	    Description
+    ALL 	        TRUE if all of the subquery values meet the condition 	
+    AND 	        TRUE if all the conditions separated by AND is TRUE 	
+    ANY 	        TRUE if any of the subquery values meet the condition 	
+    BETWEEN 	    TRUE if the operand is within the range of comparisons 	
+    EXISTS 	        TRUE if the subquery returns one or more records 	
+    IN 	            TRUE if the operand is equal to one of a list of expressions 	
+    LIKE 	        TRUE if the operand matches a pattern 	
+    NOT 	        Displays a record if the condition(s) is NOT TRUE 	
+    OR 	            TRUE if any of the conditions separated by OR is TRUE 	
+    SOME 	        TRUE if any of the subquery values meet the condition
+*/
